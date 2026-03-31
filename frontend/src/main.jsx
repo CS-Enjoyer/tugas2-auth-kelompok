2a79c7d555ddea4ab444b458a3a8e6bc6f3349a9
Ahmad Nizar Sauki <anizarsauki@gmail.com>
2026-04-01 06:36:40 +0700

    Membungkus Aplikasi

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// frontend/src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Import Provider
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { AuthProvider } from './context/AuthContext.jsx';
+
+// TODO: Ganti dengan Client ID Google milik kelompokmu
+const GOOGLE_CLIENT_ID = "MASUKKAN_CLIENT_ID_GOOGLE_DI_SINI.apps.googleusercontent.com";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </GoogleOAuthProvider>
+  </React.StrictMode>,
+);
